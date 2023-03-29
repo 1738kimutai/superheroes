c3448c43b8088505a19663adf20059bb79fc31cd
1738kimutai <davieskimutai4@gmail>
2023-03-29 11:51:00 +0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :heros, only: [:index, :show]
+  resources :powers, only: [:index, :show, :update]
+  resources :hero_powers, only: [:create]
+
+  # heroes 
+  get 'heros', to: 'heros#index'
+  get 'heros/:id', to: 'heros#show'
+end
